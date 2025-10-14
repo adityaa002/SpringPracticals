9b6ff6c7e35f2da097d50c43bbf4b6da3a3696a9
adityaa002 <aaeaditya02@gmail.com>
2025-10-14 15:39:57 +0530

    14-10-2025

---
+++ Spring-Core/src/main/java/in/co/rays/test/TestPerson.java
@@ -0,0 +1,24 @@
+package in.co.rays.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestPerson {
+	public static void main(String[] args) {
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("person.xml"));
+
+		Person p = (Person) factory.getBean("person");
+		System.out.println("Person's name : " + p.getName());
+		System.out.println("Person's address : " + p.getAddress());
+
+		System.out.println("------------------");
+
+		
+		  Person p1 = (Person) factory.getBean("person1");
+		  System.out.println("Person's name : " + p1.getName());
+		  System.out.println("Person's address : " + p1.getAddress());
+		 
+	}
+}
