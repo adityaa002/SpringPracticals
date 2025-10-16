58b32b5a2b01949f164cf9d5324c345c10cebecd
adityaa002 <aaeaditya02@gmail.com>
2025-10-16 15:49:46 +0530

    16-10-2025

--- Spring-Core/src/main/java/in/co/rays/test/TestMultiXml.java
+++ Spring-Core/src/main/java/in/co/rays/test/TestMultiXml.java
@@ -2,31 +2,45 @@ package in.co.rays.test;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class TestMultiXml {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("MultiXML.xml"));
+		/*
+		 * 
+		 * BeanFactory factory = new XmlBeanFactory(new
+		 * ClassPathResource("MultiXML.xml"));
+		 * 
+		 * // With typeCasting // Person p = (Person) factory.getBean("person");
+		 * 
+		 * // Without typeCasting Person p = factory.getBean("person", Person.class);
+		 * 
+		 * System.out.println("Person's name : " + p.getName());
+		 * System.out.println("Person's address : " + p.getAddress());
+		 * 
+		 * System.out.println("----------------------------");
+		 * 
+		 * // With typeCasting // Employee emp = (Employee) factory.getBean("employee");
+		 * 
+		 * // Without typeCasting Employee emp = factory.getBean("employee",
+		 * Employee.class);
+		 * 
+		 * System.out.println("Name :" + emp.getName()); System.out.println("Salary :" +
+		 * emp.getSalary());
+		 * 
+		 */
 
-		// With typeCasting
-		// Person p = (Person) factory.getBean("person");
-
-		// Without typeCasting
-		Person p = factory.getBean("person", Person.class);
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("MultiXML.xml");
+		Person p = context.getBean("person", Person.class);
 		System.out.println("Person's name : " + p.getName());
 		System.out.println("Person's address : " + p.getAddress());
 
-		System.out.println("----------------------------");
-
-		// With typeCasting
-		// Employee emp = (Employee) factory.getBean("employee");
-
-		// Without typeCasting
-		Employee emp = factory.getBean("employee", Employee.class);
-
+		Employee emp = context.getBean("employee", Employee.class);
 		System.out.println("Name :" + emp.getName());
 		System.out.println("Salary :" + emp.getSalary());
+
 	}
 }
