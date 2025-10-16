8d900cb70969bccde044729b45e05fad054270e1
adityaa002 <aaeaditya02@gmail.com>
2025-10-16 15:18:16 +0530

    16-10-2025

--- Spring-Core/src/main/java/in/co/rays/test/Employee.java
+++ Spring-Core/src/main/java/in/co/rays/test/Employee.java
@@ -5,6 +5,11 @@ public class Employee {
 	private String name;
 	private String salary;
 
+	public Employee(String name, String salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
 	public String getName() {
 		return name;
 	}
--- Spring-Core/src/main/java/in/co/rays/test/TestEmployee.java
+++ Spring-Core/src/main/java/in/co/rays/test/TestEmployee.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class TestEmployee {
+	
 
 	public static void main(String[] args) {
 
