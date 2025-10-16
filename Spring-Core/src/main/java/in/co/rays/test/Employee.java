95fc55c03eb1414403ac56fa85b0826be4e8ca61
adityaa002 <aaeaditya02@gmail.com>
2025-10-16 15:12:33 +0530

    16-10-2025

---
+++ Spring-Core/src/main/java/in/co/rays/test/Employee.java
@@ -0,0 +1,24 @@
+package in.co.rays.test;
+
+public class Employee {
+
+	private String name;
+	private String salary;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+}
---
+++ Spring-Core/src/main/java/in/co/rays/test/TestEmployee.java
@@ -0,0 +1,18 @@
+package in.co.rays.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("employee.xml"));
+		Employee emp = (Employee) factory.getBean("employee");
+
+		System.out.println("Name :" + emp.getName());
+		System.out.println("Salary :" + emp.getSalary());
+	}
+
+}
