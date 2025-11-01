358961f1bfa2f68e983cccc3d67b7ec09f137dac
adityaa002 <aaeaditya02@gmail.com>
2025-11-01 23:10:59 +0530

    01-11-2025

---
+++ Spring-Core/src/main/java/in/co/rays/AppConfig.java
@@ -0,0 +1,10 @@
+package in.co.rays;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("in.co.rays")
+public class AppConfig {
+
+}
---
+++ Spring-Core/src/main/java/in/co/rays/annotation/Person.java
@@ -0,0 +1,31 @@
+package in.co.rays.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("Person")
+public class Person {
+
+	@Value("Ram")
+	private String name;
+
+	@Value("Indore")
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
