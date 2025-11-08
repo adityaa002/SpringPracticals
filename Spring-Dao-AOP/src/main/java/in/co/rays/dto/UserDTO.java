3ca944414c51088d9f978462cda165c836b79eee
adityaa002 <aaeaditya02@gmail.com>
2025-11-08 19:16:56 +0530

    08-11-2025

--- Spring-Dao-AOP/src/main/java/in/co/rays/dto/UserDTO.java
+++ Spring-Dao-AOP/src/main/java/in/co/rays/dto/UserDTO.java
@@ -1,7 +1,25 @@
 package in.co.rays.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "ST_USER")
 public class UserDTO {
-	
+
+	@Id
+	@GeneratedValue(generator = "ncsPK")
+	@GenericGenerator(name = "ncsPK", strategy = "native")
+	@Column(name = "ID", unique = true, nullable = false)
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String login;
+	private String password;
 
 }
