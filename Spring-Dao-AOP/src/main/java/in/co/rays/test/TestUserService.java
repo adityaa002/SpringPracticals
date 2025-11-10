2408de9e72853fc42bdce93de131d105ba6b0ff6
adityaa002 <aaeaditya02@gmail.com>
2025-11-10 18:57:42 +0530

    10-11-2025

--- Spring-Dao-AOP/src/main/java/in/co/rays/test/TestUserService.java
+++ Spring-Dao-AOP/src/main/java/in/co/rays/test/TestUserService.java
@@ -1,5 +1,8 @@
 package in.co.rays.test;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,50 +21,85 @@ public class TestUserService {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		TestUserService service = (TestUserService) context.getBean("testUserService");
-		
-		
-		service.testAdd();
+
+//		service.testAdd();
 //		service.testUpdate();
 //		service.testDelete();
 //		service.testFindByPk();
 //		service.testFindByLogin();
 //		service.testAuth();
-//		service.testSearch();
+		service.testSearch();
 	}
 
 	private void testSearch() {
-		// TODO Auto-generated method stub
+		
+		UserDTO dto = new UserDTO();
+
+		dto.setFirstName("amit");
+		
+		List list = userService.search(dto, 1, 5);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+			dto = (UserDTO) it.next();
+			System.out.print(dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + "\t" + dto.getLastName());
+			System.out.print("\t" + "\t" + dto.getLogin());
+			System.out.println("\t" + "\t" + dto.getPassword());
+		}
 
 	}
 
 	private void testAuth() {
-		// TODO Auto-generated method stub
-
+		UserDTO dto = userService.authenticate("aditya@gmail.com", "pass123");
+		if (dto != null) {
+			System.out.print(dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + dto.getLastName());
+			System.out.print("\t" + dto.getLogin());
+			System.out.println("\t" + dto.getPassword());
+		} else {
+			System.out.println("user not found");
+		}
 	}
 
 	private void testFindByLogin() {
-		// TODO Auto-generated method stub
-
+		UserDTO dto = userService.findByLogin("aditya@gmail.com");
+		System.out.print(dto.getId());
+		System.out.print("\t" + dto.getFirstName());
+		System.out.print("\t" + dto.getLastName());
+		System.out.print("\t" + dto.getLogin());
+		System.out.println("\t" + dto.getPassword());
 	}
 
 	private void testFindByPk() {
-		// TODO Auto-generated method stub
 
+		UserDTO dto = userService.findBypk(5L);
+
+		System.out.print(dto.getId());
+		System.out.print("\t" + dto.getFirstName());
+		System.out.print("\t" + dto.getLastName());
+		System.out.print("\t" + dto.getLogin());
+		System.out.println("\t" + dto.getPassword());
 	}
 
 	private void testDelete() {
-		// TODO Auto-generated method stub
-
+		UserDTO dto = new UserDTO();
+		dto.setId(4L);
+		userService.delete(dto);
 	}
 
 	private void testUpdate() {
-		// TODO Auto-generated method stub
-
+		UserDTO dto = new UserDTO();
+		dto.setFirstName("aadityaa");
+		dto.setId(5);
+		userService.update(dto);
 	}
 
 	private void testAdd() {
 		UserDTO dto = new UserDTO();
-		dto.setId(4L);
 		dto.setFirstName("aditya");
 		dto.setLastName("sharma");
 		dto.setLogin("aaeditya@gmail.com");
