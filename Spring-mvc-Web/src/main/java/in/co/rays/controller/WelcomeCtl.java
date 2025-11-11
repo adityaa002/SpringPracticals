e0aecd940cc5801ba03c259ba0992be948f47f67
adityaa002 <aaeaditya02@gmail.com>
2025-11-11 13:59:33 +0530

    11-11-2025

--- Spring-mvc-Web/src/main/java/in/co/rays/controller/WelcomeCtl.java
+++ Spring-mvc-Web/src/main/java/in/co/rays/controller/WelcomeCtl.java
@@ -1,8 +1,18 @@
 package in.co.rays.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class WelcomeCtl {
 
+	@GetMapping
+	public String display(Model model) {
+		model.addAttribute("message", "Welcome to Spring MVC...!");
+		return "WelcomeView";
+
+	}
 }
