c23bd356afadcf43a12aa178bc36eda58f367d27
adityaa002 <aaeaditya02@gmail.com>
2025-11-12 13:18:19 +0530

    12-11-2025

---
+++ Spring-mvc-Web/src/main/java/in/co/rays/controller/UserRegistrationCtl.java
@@ -0,0 +1,12 @@
+package in.co.rays.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController(value = "/UserRegistration")
+public class UserRegistrationCtl {
+	
+	
+	
+}
