130f6b8ef437f4615862a6f541996b6b5103ff3e
adityaa002 <aaeaditya02@gmail.com>
2025-11-12 13:35:16 +0530

    12-11-2025

--- Spring-mvc-Web/src/main/java/in/co/rays/controller/UserRegistrationCtl.java
+++ Spring-mvc-Web/src/main/java/in/co/rays/controller/UserRegistrationCtl.java
@@ -1,12 +1,20 @@
 package in.co.rays.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.co.rays.form.UserRegistrationForm;
 
 @Controller
 @RestController(value = "/UserRegistration")
 public class UserRegistrationCtl {
-	
-	
-	
+
+	@GetMapping
+	public String display(@ModelAttribute("form") UserRegistrationForm form) {
+		System.out.println("in get display method");
+		return "UserRegistrationView";
+	}
+
 }
