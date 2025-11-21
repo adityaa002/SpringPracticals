dc86710b38fe92ded90d75ecdc88e155c1d3a9fd
adityaa002 <aaeaditya02@gmail.com>
2025-11-21 16:48:28 +0530

    21-11-2025

--- Spring-boot/src/main/java/in/co/rays/controller/RoleCtl.java
+++ Spring-boot/src/main/java/in/co/rays/controller/RoleCtl.java
@@ -1,10 +1,57 @@
 package in.co.rays.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.co.rays.common.BaseCtl;
 import in.co.rays.common.ORSResponse;
+import in.co.rays.dto.RoleDTO;
+import in.co.rays.form.RoleForm;
+import in.co.rays.service.RoleServiceInt;
 
+@RestController
+@RequestMapping("Role")
 public class RoleCtl extends BaseCtl {
+
+	@Autowired
+	public RoleServiceInt roleService;
+
+	@PostMapping("save")
+	public ORSResponse save(@RequestBody @Valid RoleForm form, BindingResult bindingResult) {
+
+		ORSResponse res = validate(bindingResult);
+
+		if (!res.isSuccess()) {
+			return res;
+		}
+
+		RoleDTO dto = (RoleDTO) form.getDto();
+
+		long pk = roleService.add(dto);
+
+		res.addData(pk);
+		res.addMessage("Role Added Succesfully");
+
+		return res;
+
+	}
+
+	@PostMapping("update")
+	public ORSResponse update(@RequestBody @Valid RoleForm form, BindingResult bindingResult) {
+
+		ORSResponse res = validate(bindingResult);
+
+		if (!res.isSuccess()) {
+			return res;
+		}
+		return null;
+		
+	}
 
 }
