815d7bff011af32e0914d5617fc29b86571d1a2c
adityaa002 <aaeaditya02@gmail.com>
2025-11-26 18:00:13 +0530

    26-11-2025

--- Spring-boot/src/main/java/in/co/rays/controller/LoginCtl.java
+++ Spring-boot/src/main/java/in/co/rays/controller/LoginCtl.java
@@ -36,6 +36,7 @@ public class LoginCtl extends BaseCtl {
 		
 		UserDTO dto = userService.authenticate(form.getLoginId(), form.getPassword());
 		if (dto != null) {
+			
 			session.setAttribute("user", dto);
 			res.addData(dto);
 			res.addMessage("User Login Successfully...!!");
